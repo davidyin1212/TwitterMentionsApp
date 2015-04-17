c084083d8c7dd638e8527062274c563ccd3b7e8d
davidyin1212 <davidyin1212@gmail.com>
2015-04-17 01:51:29 -0400

    post migration commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_from_auth_hash(auth_hash)
-  	raise :test    
-    self.current_user = @user
+    @user = User.find_or_create_from_auth_hash(auth_hash)    
+    session[:user_id] = @user.id
     redirect_to root_path
   end
 
